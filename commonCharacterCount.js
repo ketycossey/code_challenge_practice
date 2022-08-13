b3a5b114053e44d66d997469748bd50950494393
ketycossey <54608435+ketycossey@users.noreply.github.com>
2022-08-13 12:26:01 -0500

    common character count

---
+++ commonCharacterCount.js
@@ -0,0 +1,17 @@
+//Given two strings, find the number of common characters between them.
+
+function commonCharacterCount(s1, s2){
+    var count = 0
+    for(var i = 0; i < s1.length; i++){
+          var j = s2.indexOf(s1[i])
+        if (j >= 0){
+            count ++
+            s2 = s2.replace(s1[i], '')
+          
+        }
+    }
+    return count
+}
+
+console.log(commonCharacterCount('locotoco', 'tomococo'))
+console.log(commonCharacterCount('aabcc', 'adcaa'))
--- longestString.js
+++ longestString.js
@@ -4,11 +4,23 @@
 // solution(inputArray) = ["aba", "vcd", "aba"].
 
 function longestString(inputArray){
-    longestWord=''
-    for(var str of inputArray){
-        if(str.length > longestWord.length)
-        longestWord = str
-    }return longestWord
+    var newArray=[]
+    
+    if(inputArray.length == 1){
+        return inputArray
+    }
+    var longestWord = inputArray.sort(function(a,b) {
+        return b.length - a.length
+       // console.log(longestWord)
+    })
+    var longestWordLength= longestWord[0].length
+
+    for( let i = 0; i < longestWord.length; i++){
+        if(longestWord[i].length== longestWordLength){
+            newArray.push(longestWord[i])
+        }
+     
+    }return newArray
 
 }
 console.log(longestString(['aaa', 'bbbbb', 'ccccc', 'ddddd', 'ee', 'lllll']))
